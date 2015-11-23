d2a90382535e861cd46ad717d739d5bd3f0833d5
josemlopezm <josem.lopezm@gmail.com>
2015-11-23 16:40:24 +0100

    Aliases with Colors of grep command

---
+++ colorGreps.sh
@@ -0,0 +1,8 @@
+alias egrep-cyan="GREP_COLOR='1;36' egrep --line-buffered --color=always"
+alias egrep-green="GREP_COLOR='1;32' egrep --line-buffered --color=always"
+alias egrep-red="GREP_COLOR='1;31' egrep --line-buffered --color=always"
+alias egrep-brow="GREP_COLOR='1;38' egrep --line-buffered --color=always"
+alias egrep-yellow="GREP_COLOR='00;38;5;226' egrep --line-buffered --color=always"
+alias egrep-purple="GREP_COLOR='1;35' egrep --line-buffered --color=always"
+alias egrep-blue="GREP_COLOR='1;34' egrep --line-buffered --color=always"
+alias egrep-redBack="GREP_COLOR='30;48;5;196' egrep --line-buffered --color=always"
